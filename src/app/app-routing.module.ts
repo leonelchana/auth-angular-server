c0023fd2f0163334d2419fec54b535fb3b83bbee
sistemasfinorleonel <96667421+sistemasfinorleonel@users.noreply.github.com>
2023-02-17 18:22:34 -0400

    terminada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes
+    /* ,{
+    useHash:false
+  } */
+  )],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
